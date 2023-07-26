4e2296b297fb887ceb10aae4b7e93d5aa1459bcb
Mateus Moraes <mmds89@gmail.com>
2023-07-26 10:26:13 -0300

    feat: add cpf/cnpj validators

--- src/models/dto/rural-producer-create.dto.ts
+++ src/models/dto/rural-producer-create.dto.ts
@@ -1,7 +1,13 @@
+import { IsNotEmpty, Matches, Validate } from 'class-validator';
+import { IsCpfCnpj } from '../../validators/cpfCnpj.validator';
+
 export class RuralProducerCreateDto {
 
+    @IsNotEmpty()
     name: string;
 
+
+    @Validate(IsCpfCnpj)
     cpfOrCnpj: string;
 
 }
