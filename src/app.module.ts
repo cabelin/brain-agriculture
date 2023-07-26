7e4ca7180c4d5fe0c468e066346fa126bf2d8fd0
Mateus Moraes <mmds89@gmail.com>
2023-07-26 20:26:31 -0300

    feat: add report dashboard api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RuralProducerModule } from './modules/rural-producer.module';
 import { typeOrmModuleOptions } from '../db/data-source';
+import { RuralProducerModule } from './modules/rural-producer.module';
+import { ReportModule } from './modules/report.module';
 
 @Module({
   imports: [
     RuralProducerModule,
+    ReportModule,
     TypeOrmModule.forRoot(typeOrmModuleOptions),
   ],
   controllers: [AppController],
